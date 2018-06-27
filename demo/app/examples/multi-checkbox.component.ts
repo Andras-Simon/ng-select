b88a9ef111ed79ffb6becb978af6c94a069a460f
Andras <andras@cabsolutely.co>
2018-06-27 19:52:25 +0200

    Fix

--- demo/app/examples/multi-checkbox.component.ts
+++ demo/app/examples/multi-checkbox.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { DataService, Person } from '../shared/data.service';
-import { map } from 'rxjs/operators';
 
 @Component({
     encapsulation: ViewEncapsulation.None,
@@ -37,7 +36,6 @@ export class SelectMultiCheckboxComponent {
 
     ngOnInit() {
         this.dataService.getPeople()
-        .pipe(map(x => x.filter(y => !y.disabled)))
         .subscribe((res) => {
             this.people = res;
             this.selectedPeople = [this.people[0].id, this.people[1].id];
